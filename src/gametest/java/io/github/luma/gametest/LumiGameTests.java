d8d0f7a822376636198ddb68b6d7285432cbb9e2
Zlo <kostos2001@gmail.com>
2026-04-21 06:09:10 +0300

    test(gametest): add headless client scaffold

---
+++ src/gametest/java/io/github/luma/gametest/LumiGameTests.java
@@ -0,0 +1,22 @@
+package io.github.luma.gametest;
+
+import java.lang.reflect.Method;
+import net.fabricmc.fabric.api.gametest.v1.CustomTestMethodInvoker;
+import net.fabricmc.fabric.api.gametest.v1.GameTest;
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.world.level.block.Blocks;
+
+public final class LumiGameTests implements CustomTestMethodInvoker {
+
+    @GameTest
+    public void serverHarnessBoots(GameTestHelper context) {
+        context.assertBlockPresent(Blocks.AIR, 0, 0, 0);
+        context.succeed();
+    }
+
+    @Override
+    public void invokeTestMethod(GameTestHelper context, Method method) throws ReflectiveOperationException {
+        context.setBlock(0, 0, 0, Blocks.AIR);
+        method.invoke(this, context);
+    }
+}
