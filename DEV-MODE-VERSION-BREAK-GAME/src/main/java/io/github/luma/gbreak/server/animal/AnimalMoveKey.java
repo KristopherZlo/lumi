c48041cc00fd6ebf7d3b01d5495d67fa707e414c
Zlo <kostos2001@gmail.com>
2026-04-25 21:27:11 +0300

    Remove nested DEV-MODE project

--- DEV-MODE-VERSION-BREAK-GAME/src/main/java/io/github/luma/gbreak/server/animal/AnimalMoveKey.java
+++
@@ -1,8 +0,0 @@
-package io.github.luma.gbreak.server.animal;
-
-import java.util.UUID;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.world.World;
-
-record AnimalMoveKey(RegistryKey<World> worldKey, UUID animalId) {
-}
