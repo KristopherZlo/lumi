3e99f1e0ccfcf1c8ea24b3fd4663a9816c11a0d2
Zlo <kostos2001@gmail.com>
2026-04-25 19:08:43 +0300

    Revert "Add animal move session primitives"
    This reverts commit 694f614be46c215e15cc8a5e4bd06411efd6fdb1.

--- src/main/java/io/github/luma/minecraft/animal/AnimalMoveKey.java
+++
@@ -1,8 +0,0 @@
-package io.github.luma.minecraft.animal;
-
-import java.util.UUID;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.Level;
-
-record AnimalMoveKey(ResourceKey<Level> levelKey, UUID animalId) {
-}
