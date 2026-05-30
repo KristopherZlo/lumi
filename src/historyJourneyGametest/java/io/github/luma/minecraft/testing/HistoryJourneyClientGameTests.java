ce1d5fb7e39b3e3b2e5fd46db457e6b2e864a196
Zlo <kostos2001@gmail.com>
2026-05-30 08:36:47 +0300

    test: add history journey gametest entrypoint

---
+++ src/historyJourneyGametest/java/io/github/luma/minecraft/testing/HistoryJourneyClientGameTests.java
@@ -0,0 +1,13 @@
+package io.github.luma.minecraft.testing;
+
+import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
+import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
+
+@SuppressWarnings("UnstableApiUsage")
+public final class HistoryJourneyClientGameTests implements FabricClientGameTest {
+
+    @Override
+    public void runTest(ClientGameTestContext context) {
+        throw new AssertionError("history journey scenario not implemented");
+    }
+}
