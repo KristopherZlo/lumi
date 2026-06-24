4b58bd85226bb8d8acc23ee39ebdb1de24470e3d
Zlo <kostos2001@gmail.com>
2026-06-24 04:12:37 +0300

    Remove obsolete external tool adapters

--- src/main/java/io/github/luma/integration/common/SchematicBridge.java
+++
@@ -1,8 +0,0 @@
-package io.github.luma.integration.common;
-
-import java.util.List;
-
-public interface SchematicBridge extends ExternalToolAdapter {
-
-    List<String> supportedSchematicFormats();
-}
